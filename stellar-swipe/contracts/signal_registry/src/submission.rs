f47196a7457e5c307f894a4cd7d49d4404feb6f2
Ajidokwu Sabo <realjaiboi70@gmail.com>
2026-01-24 17:04:01 +0100

    submission

--- stellar-swipe/contracts/signal_registry/src/submission.rs
+++ stellar-swipe/contracts/signal_registry/src/submission.rs
@@ -3,3 +3,13 @@
 use soroban_sdk::{contracttype, Address, Env, Map, Vec};
 
 use crate::stake::{can_submit_signal, ContractError, StakeInfo, DEFAULT_MINIMUM_STAKE, UNSTAKE_LOCK_PERIOD};
+
+/// Action enum for trading signals
+#[contracttype]
+#[derive(Clone)]
+pub enum Action {
+    Buy,
+    Sell,
+    Hold,
+}
+
